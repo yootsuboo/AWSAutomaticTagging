7684273e0e32e90f083baf44c53a90dd80bf1df3
ootsubo1192 <ootubo_stock@yahoo.co.jp>
2022-10-30 06:32:08 +0900

    modify Automatic-Logging-Lambda.py

--- Lambda/Automatic-Logging-Lambda.py
+++ Lambda/Automatic-Logging-Lambda.py
@@ -2,18 +2,13 @@ import boto3, json
 
 def lambda_handler(event, content):
 
-    # 構築されたインスタンスID
     instanceId = event['detail']['responseElements']['instancesSet']['items'][0]['instanceId']
 
-    # リソース作成者のユーザー情報
-    userIdentity = event['detail']['userIdentity']
-    userName = userIdentity['userName']
+    userName = event['detail']['userIdentity']['userName']
     
-    # リソース作成日の情報
-    creationDate = userIdentity['sessionContext']['attributes']['creationDate'][:10]
+    creationDate = event['detail']['userIdentity']['sessionContext']['attributes']['creationDate'][:10]
     
     
-    # 構築されたインスタンスへのタグ付け
     ec2 = boto3.client('ec2')
     ec2.create_tags(
         Resources=[instanceId,],
